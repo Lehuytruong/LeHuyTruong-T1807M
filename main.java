69fb44b4f1ea2f6a6f0bb1d5da1009863a31a42f
Lehuytruong <46685124+Lehuytruong@users.noreply.github.com>
2019-03-15 21:16:33 +0700

    Add files via upload

--- main.java
+++ main.java
@@ -1,10 +1,11 @@
-package Asingment02;
+package lap_session3;
 
 public class main {
-    public static void main(String args[]) {
-        Student Student = new Student();
-        Student.inputInfo();
-        Student.showInfo();
-        Student.chekReward();
+    public static void main(String args[]){
+        SoNguyenTo snt = new SoNguyenTo(4);
+
+        for (int i=0;i<30;i++){
+            System.out.println("SNT:"+snt.timSoNguyenToTiepTheo());
+        }
     }
 }
