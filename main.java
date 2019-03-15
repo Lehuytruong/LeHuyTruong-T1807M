ebb61da500cf2008951886b89810905070fdbf0a
Lehuytruong <46685124+Lehuytruong@users.noreply.github.com>
2019-03-15 21:06:30 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,11 @@
+package lap_session3;
+
+public class main {
+    public static void main(String args[]){
+        SoNguyenTo snt = new SoNguyenTo(4);
+
+        for (int i=0;i<30;i++){
+            System.out.println("SNT:"+snt.timSoNguyenToTiepTheo());
+        }
+    }
+}
